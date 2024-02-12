c4d7ce7b2773e8c0b7b0caeb6f6fb4f45aa37a89
bharathspt <159775945+bharathspt@users.noreply.github.com>
2024-02-12 16:37:21 +0530

    Update one.java

--- one.java
+++ one.java
@@ -4,7 +4,7 @@ class hello
 	{
 		system.out.print ln ("helloword");
 		system.out.print ln ("helloword java");
-	        system.out.print ln ("helloword java,spt");
+	        system.out.print ln ("helloword java,spt1234567890");
 	}
 }
 
